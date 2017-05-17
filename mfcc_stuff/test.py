ae5a92ed57bc0b0aad36cf257efd2c39bda110ad
Shalom Rottman-Yang <shalom@DN51ut30.SUNet>
2017-05-16 22:00:41 -0700

    updated test.py to make mfcc + delta mfcc features

--- mfcc_stuff/test.py
+++ mfcc_stuff/test.py
@@ -8,8 +8,6 @@ import scipy.io.wavfile as wav
 (rate,sig) = wav.read("Queen.wav")
 mfcc_feat = mfcc(sig,rate)
 d_mfcc_feat = delta(mfcc_feat, 2)
-fbank_feat = logfbank(sig,rate)
+#fbank_feat = logfbank(sig,rate)
 
-print(fbank_feat[0])
-print d_mfcc_feat
-print mfcc_feat
+print [a + b for a, b in zip(mfcc_feat, d_mfcc_feat)]
