c2772c39a0ce26cb41455dfa6e43f9f0a46f3de0
Shalom Rottman-Yang <shalom@DN51ut30.SUNet>
2017-05-16 21:03:04 -0700

    trying out python_speech_features

---
+++ mfcc_stuff/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from python_speech_features import mfcc
+from python_speech_features import delta
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+
+(rate,sig) = wav.read("Queen.wav")
+mfcc_feat = mfcc(sig,rate)
+d_mfcc_feat = delta(mfcc_feat, 2)
+fbank_feat = logfbank(sig,rate)
+
+print(fbank_feat[0])
+print d_mfcc_feat
+print mfcc_feat
