8be84f763bb7868bfc065a9ad6a146d555094c43
Teddy Morris-Knower <ted107mk@gmail.com>
2017-05-16 23:51:39 -0700

    renamed, added validation

--- mfcc_stuff/wav_to_mfcc.py
+++ mfcc_stuff/wav_to_mfcc.py
@@ -23,14 +23,21 @@ def read_files():
 	start = time.time() #timing
 	all_features = [] #eventually use numpy?
 	seq_lengths = []
+	all_features_val = [] #eventually use numpy?
+	seq_lengths_val = []
+	num_files = len(files)
 	for i,file in enumerate(files):
 		(rate,sig) = wav.read(artic_dir + "/" + file) 
 		mfcc_feat = mfcc(sig,rate) #can add more features later but for now just mfcc_feat
 		# d_mfcc_feat = delta(mfcc_feat, 2) 
 		# fbank_feat = logfbank(sig,rate)
 		# features.add(mfcc_feat)
-		all_features.append(mfcc_feat)
-		seq_lengths.append(len(mfcc_feat))
+		if i < (num_files/10):
+			all_features_val.append(mfcc_feat)
+			seq_lengths_val.append(len(mfcc_feat))
+		else:
+			all_features.append(mfcc_feat)
+			seq_lengths.append(len(mfcc_feat))
 		# if i % 100 == 0:
 		# 	print "File number:", i
 		# 	print time.time() - start
@@ -38,20 +45,30 @@ def read_files():
 	label_dir = cwd + "/../cmu_labels/cmu_us_bdl_arctic"
 	files = [f for f in listdir(label_dir)]
 	all_labels = []
+	all_labels_val = []
 	for i,file in enumerate(files):
 		label = []
 		with open(label_dir + "/" + file, 'r') as f:
 			for line in f.readlines():
 				label += line.split()
-		all_labels.append(" ".join(label))
+		if i < (num_files/10):	
+			all_labels_val.append(" ".join(label))
+		else:
+			all_labels.append(" ".join(label))
 		# if i % 100 == 0:
 		# 	print "File number:", i
 		# 	print time.time() - start
 
 	pickle_tuple = (all_features, seq_lengths, all_labels)
+	# print (len(all_features), len(seq_lengths), len(all_labels))
 	# print pickle_tuple
-	with open('mfcc_features', 'wb') as f:
+	pickle_tuple_val = (all_features_val, seq_lengths_val, all_labels_val)
+	# print (len(all_features_val), len(seq_lengths_val), len(all_labels_val))
+	with open('cmu_train.dat', 'wb') as f:
 		pickle.dump(pickle_tuple, f)
+	with open('cmu_val.dat', 'wb') as f:
+		pickle.dump(pickle_tuple_val, f)
+
 
 
 read_files()
