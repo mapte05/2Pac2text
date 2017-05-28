d2921aa557d3ed61a2880127f983327b94099646
Maneesh Apte <maneesh@kanjoya.com>
2017-05-28 14:01:45 -0700

    modify train.sh slightly and delete saved models and tensorboard files

--- train.sh
+++ train.sh
@@ -1,4 +1,9 @@
 # bash script that encapsulates control flow for training the model
 echo 'starting training'
+
+# run with printing predicted sequence versus actual
 python model.py --save_every 1 --print_every 1
+
+# run without printing predicted
+python model.py --save_every 1
 
