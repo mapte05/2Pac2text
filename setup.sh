f69b6f1a5a5fd0e0af79282e9a5d7978874500a0
Maneesh Apte <maneesh@kanjoya.com>
2017-06-06 04:13:32 -0700

    install tensorflow in root dir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 echo 'Assuming this is Ubuntu 16.04 LTS'
 echo 'Installing tensorflow...'
 sudo apt-get install python-pip python-dev python-virtualenv 
-virtualenv --system-site-packages tensorflow
-source tensorflow/bin/activate
+virtualenv --system-site-packages ~/tensorflow
+source ~/tensorflow/bin/activate
 pip install --upgrade tensorflow
