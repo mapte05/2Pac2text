94730d680198d5ce7c23029ad0622039941ebfc4
Matthew I. W. Chen <mchen.ja@gmail.com>
2017-06-06 01:36:06 -0700

    create setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo 'Assuming this is Ubuntu 16.04 LTS'
+echo 'Installing tensorflow...'
+sudo apt-get install python-pip python-dev python-virtualenv 
+virtualenv --system-site-packages tensorflow
+source ~/tensorflow/bin/activate
+sudo pip install --upgrade tensorflow
