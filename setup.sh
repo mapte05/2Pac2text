2e35c351a8f7a6601745f19923e00ebd5db11199
Matthew I. W. Chen <mchen.ja@gmail.com>
2017-06-06 02:09:55 -0700

    fix setup bug

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ echo 'Assuming this is Ubuntu 16.04 LTS'
 echo 'Installing tensorflow...'
 sudo apt-get install python-pip python-dev python-virtualenv 
 virtualenv --system-site-packages tensorflow
-source ~/tensorflow/bin/activate
+source tensorflow/bin/activate
 sudo pip install --upgrade tensorflow
