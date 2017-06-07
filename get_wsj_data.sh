a8525e68135ccbe773f7bc9789ee8610914d077f
Matthew I. W. Chen <mchen.ja@gmail.com>
2017-06-06 20:24:12 -0700

    update wsj links

--- get_wsj_data.sh
+++ get_wsj_data.sh
@@ -4,15 +4,12 @@ mkdir -p data/wsj
 
 # get wsj from our google drive
 echo wsj_test.dat
-wget https://doc-10-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/jks947oimedsgnvfj33l6u1ppbgk5sc5/1496728800000/13452926836532438358/*/0B38oulL41I1NVGlqZDdkcmZ0X1k?e=download
-mv 0B38oulL41I1NVGlqZDdkcmZ0X1k?e=download data/wsj/wsj_test.dat
+wget -O wsj_test.dat https://doc-0g-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/tcrde8mbcfo0j28i025nckpnfdr1tciv/1496800800000/13452926836532438358/*/0B38oulL41I1NZGlYc3Z0OTNJY28?e=download
 
 echo wsj_train.dat
-wget https://doc-0s-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/kmp721pdsit78aplbp408cev6k52r45q/1496728800000/13452926836532438358/*/0B38oulL41I1NRTg5eER0ZHU2ME0?e=download
-mv 0B38oulL41I1NRTg5eER0ZHU2ME0?e=download data/wsj/wsj_train.dat
+wget -O wsj_train.dat https://doc-04-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/74ba472de4kdb9siphjotebqta3833th/1496800800000/13452926836532438358/*/0B38oulL41I1Nb3ppSVJVUkxQNGs?e=download
 
 echo wsj_dev.dat
-wget https://doc-0g-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1osdargmpvelb8uo7m5ic8bf6gesvl9q/1496728800000/13452926836532438358/*/0B38oulL41I1NV2RjUDJlRjlxWUk?e=download
-mv 0B38oulL41I1NV2RjUDJlRjlxWUk?e=download data/wsj/wsj_dev.dat
+wget -O wsj_dev.dat https://doc-0s-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/ujf2ipto6rk57cecjbk72rl06up1eaq3/1496800800000/13452926836532438358/*/0B38oulL41I1NbXpSTnlzcS14LWc?e=download
 
 
