b1fd6a89db0d9785421b212396513f7284652420
Matthew I. W. Chen <mchen.ja@gmail.com>
2017-06-07 14:07:28 -0700

    just for maneesh

--- get_cmu_data.sh
+++ get_cmu_data.sh
@@ -2,58 +2,57 @@
 mkdir -p data
 mkdir -p data/cmu
 
-
-echo Getting cmu_all6_val.dat
-wget -O cmu_all6_val.dat https://doc-04-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/tb3go77epa6f5u0b7e9n7ur10gm6darv/1496808000000/13452926836532438358/*/0B38oulL41I1NeGktNzJraFJ4YUE?e=download
-mv cmu_all6_val.dat data/cmu/cmu_all6_val.dat
-echo -------------
+# echo Getting cmu_all6_val.dat
+# wget -O cmu_all6_val.dat https://doc-04-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/tb3go77epa6f5u0b7e9n7ur10gm6darv/1496808000000/13452926836532438358/*/0B38oulL41I1NeGktNzJraFJ4YUE?e=download
+# mv cmu_all6_val.dat data/cmu/cmu_all6_val.dat
+# echo -------------
 
 echo Getting cmu_all6_val_noisy.dat
-wget -O cmu_all6_val_noisy.dat https://doc-0g-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/ksej6aj2l6o880b3046iagmpo6ehdtof/1496808000000/13452926836532438358/*/0B38oulL41I1NenN5MVFqenJJSkk?e=download
+wget -O cmu_all6_val_noisy.dat https://doc-0g-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/47kd6eh1vh2enl3uu0t7h60r3f5b7keh/1496865600000/13452926836532438358/*/0B38oulL41I1NenN5MVFqenJJSkk?e=download
 mv cmu_all6_val_noisy.dat data/cmu/cmu_all6_val_noisy.dat
 echo -------------
 
-echo Getting cmu_all6_train.dat
-wget -O cmu_all6_train.dat https://doc-0o-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/377p3m7ffce12mo4f41tpuq6s1kvn2ad/1496808000000/13452926836532438358/*/0B38oulL41I1NbHE2b25DbXZ6TU0?e=download
-mv cmu_all6_train.dat data/cmu/cmu_all6_train.dat
-echo -------------
+# echo Getting cmu_all6_train.dat
+# wget -O cmu_all6_train.dat https://doc-0o-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/377p3m7ffce12mo4f41tpuq6s1kvn2ad/1496808000000/13452926836532438358/*/0B38oulL41I1NbHE2b25DbXZ6TU0?e=download
+# mv cmu_all6_train.dat data/cmu/cmu_all6_train.dat
+# echo -------------
 
 echo Getting cmu_all6_train_noisy.dat
-wget -O cmu_all6_train_noisy.dat https://doc-04-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/rqvjr9pbsp7u5jreki79opuf8mopoj55/1496808000000/13452926836532438358/*/0B38oulL41I1NdnJWYk8wSEJ2UGc?e=download
+wget -O cmu_all6_train_noisy.dat https://doc-04-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/41f8e9f0mslv45ij8t2rnj279gqo6u9v/1496865600000/13452926836532438358/*/0B38oulL41I1NdnJWYk8wSEJ2UGc?e=download
 mv cmu_all6_train_noisy.dat data/cmu/cmu_all6_train_noisy.dat
 echo -------------
 
 
-echo Getting cmu_all12_val.dat
-wget -O cmu_all12_val.dat https://doc-0o-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/fj37tiu5g03m2ijg3j0as20sfdr9me41/1496808000000/13452926836532438358/*/0B38oulL41I1NdDlmM19lR0tEQUU?e=download
-mv cmu_all12_val.dat data/cmu/cmu_all12_val.dat
-echo -------------
+# echo Getting cmu_all12_val.dat
+# wget -O cmu_all12_val.dat https://doc-0o-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/fj37tiu5g03m2ijg3j0as20sfdr9me41/1496808000000/13452926836532438358/*/0B38oulL41I1NdDlmM19lR0tEQUU?e=download
+# mv cmu_all12_val.dat data/cmu/cmu_all12_val.dat
+# echo -------------
 
 
-echo Getting cmu_all12_train.dat
-wget -O cmu_all12_train.dat https://doc-0k-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/j366tkko2qauu3kcmoneadcud556s1sq/1496808000000/13452926836532438358/*/0B38oulL41I1NRGtYVzNQeWhpTVk?e=download
-mv cmu_all12_train.dat data/cmu/cmu_all12_train.dat
-echo -------------
+# echo Getting cmu_all12_train.dat
+# wget -O cmu_all12_train.dat https://doc-0k-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/j366tkko2qauu3kcmoneadcud556s1sq/1496808000000/13452926836532438358/*/0B38oulL41I1NRGtYVzNQeWhpTVk?e=download
+# mv cmu_all12_train.dat data/cmu/cmu_all12_train.dat
+# echo -------------
 
 echo Getting cmu_6_warped_val.dat
-wget -O cmu_6_warped_val.dat https://doc-10-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/ci5f4f9jmhp4f7stj27enpmfvmcqbrac/1496808000000/13452926836532438358/*/0B38oulL41I1NMjBCckdocE5JN1E?e=download
+wget -O cmu_6_warped_val.dat https://doc-10-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/e9s11a8h35atd6i37mumgocb1th6j7bf/1496865600000/13452926836532438358/*/0B38oulL41I1NMjBCckdocE5JN1E?e=download
 mv cmu_6_warped_val.dat data/cmu/cmu_6_warped_val.dat
 echo -------------
 
 echo Getting cmu_6_warped_train.dat
-wget -O cmu_6_warped_train.dat https://doc-0k-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/3dbqiiql7g3almm1aa4i92iqdbtg8g19/1496808000000/13452926836532438358/*/0B38oulL41I1NNUNYanZRZWVDSEU?e=download
+wget -O cmu_6_warped_train.dat https://doc-0k-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1ubk5hgje512n7h6s5s8uqrq6ojig6s9/1496865600000/13452926836532438358/*/0B38oulL41I1NNUNYanZRZWVDSEU?e=download
 mv cmu_6_warped_train.dat data/cmu/cmu_6_warped_train.dat
 echo -------------
 
-echo Getting cmu_18_warped_noisy_val.dat
-wget -O cmu_18_warped_noisy_val.dat https://doc-0g-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1s8bk5or2kb9r76q74rf5r14ka7tapc2/1496808000000/13452926836532438358/*/0B38oulL41I1NdVo1czcwNnNyTTQ?e=download
-mv cmu_18_warped_noisy_val.dat data/cmu/cmu_18_warped_noisy_val.dat
-echo ------------
+# echo Getting cmu_18_warped_noisy_val.dat
+# wget -O cmu_18_warped_noisy_val.dat https://doc-0g-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1s8bk5or2kb9r76q74rf5r14ka7tapc2/1496808000000/13452926836532438358/*/0B38oulL41I1NdVo1czcwNnNyTTQ?e=download
+# mv cmu_18_warped_noisy_val.dat data/cmu/cmu_18_warped_noisy_val.dat
+# echo ------------
 
-echo Getting cmu_18_warped_noisy_train.dat
-wget -O cmu_18_warped_noisy_train.dat https://doc-14-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/qeveq8jtkdb4poofiaopfvafjt0j2cjc/1496808000000/13452926836532438358/*/0B38oulL41I1NWnBqTVVfaDhFVWc?e=download
-mv cmu_18_warped_noisy_train.dat data/cmu/cmu_18_warped_noisy_train.dat
-echo ------------
+# echo Getting cmu_18_warped_noisy_train.dat
+# wget -O cmu_18_warped_noisy_train.dat https://doc-14-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/qeveq8jtkdb4poofiaopfvafjt0j2cjc/1496808000000/13452926836532438358/*/0B38oulL41I1NWnBqTVVfaDhFVWc?e=download
+# mv cmu_18_warped_noisy_train.dat data/cmu/cmu_18_warped_noisy_train.dat
+# echo ------------
 
 
  
